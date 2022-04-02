fc219337c885f43ce3b285d3321ca2498c7a2839
bzoli40 <birkaszoltan.01@gmail.com>
2022-04-02 23:24:19 +0200

    Új tesztkörnyezet + első épület

---
+++ Assets/Scripts/Systems/Movement/MovementSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementSystem : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+}
