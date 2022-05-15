ac562ae1674c9944da6050ba133f996bd2531707
bzoli40 <birkaszoltan.01@gmail.com>
2022-05-15 15:36:48 +0200

    f

--- Assets/Scripts/Systems/AI/Entities/ZombieAI.cs
+++ Assets/Scripts/Systems/AI/Entities/ZombieAI.cs
@@ -6,13 +6,18 @@ using UnityEngine.AI;
 public class ZombieAI : MonoBehaviour
 {
     private NavMeshAgent nav_agent;
-    private Vector3 nav_destin = new(0,0,0);
 
     [Header("AI beállítások")]
     public float posUpdateDistance;
+
+    public float attackRange;
+    public Vector3 attackRangeOffset;
     public float followRange;
 
-    public AIMode ai_mode;
+    //public AIMode ai_mode;
+
+    public bool isAttacking = false;
+    public bool isMoving = false;
 
     private void Awake()
     {
@@ -21,21 +26,33 @@ public class ZombieAI : MonoBehaviour
 
     private void Update()
     {
-        CheckForPlayerNearby();
-
-        if (GameManager.main.gameState == GameState.PLAY)
+        if(GameManager.main.gameState == GameState.PLAY)
         {
-            switch (ai_mode)
-            {
-                case AIMode.FOLLOW:
-                    Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
-                    if (Vector3.Distance(playerPos, nav_destin) >= posUpdateDistance) nav_destin = playerPos;
-                    nav_agent.destination = playerPos;
-                    break;
+            CheckForPlayerForHit();
 
-                case AIMode.STANDBY:
-                    nav_agent.destination = transform.position;
-                    break;
+            if (!isAttacking) CheckForPlayerNearby();
+            else isMoving = false;
+
+            if (isAttacking)
+            {
+                Debug.Log("Támad!");
+            }
+            else if (isMoving)
+            {
+                Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+                if (Vector3.Distance(playerPos, nav_agent.destination) >= posUpdateDistance)
+                {
+                    nav_agent.destination = playerPos;
+                }
+
+                if (Vector3.Distance(transform.position, nav_agent.destination) <= posUpdateDistance)
+                {
+                    isMoving = false;
+                }
+            }
+            else if (!isMoving)
+            {
+                nav_agent.destination = transform.position;
             }
         }
     }
@@ -54,12 +71,55 @@ public class ZombieAI : MonoBehaviour
             }
         }
 
-        ai_mode = isTherePlayer ? AIMode.FOLLOW : AIMode.STANDBY;
+        if(isMoving && !isTherePlayer)
+        {
+            GetComponent<Animator>().SetTrigger("stop");
+            isMoving = false;
+        }
+        else if(!isMoving && isTherePlayer)
+        {
+            GetComponent<Animator>().SetTrigger("move");
+            isMoving = true;
+        }
+    }
+
+    private void CheckForPlayerForHit()
+    {
+        Collider[] hitters = Physics.OverlapSphere(transform.position + attackRangeOffset, attackRange);
+
+        bool isTherePlayer = false;
+
+        foreach (Collider hitter in hitters)
+        {
+            if (hitter.tag == "Player" || hitter.GetComponent<IsPlayer>() != null)
+            {
+                isTherePlayer = true;
+            }
+        }
+
+        if (isTherePlayer)
+        {
+            GetComponent<Animator>().SetTrigger("attack");
+            isAttacking = true;
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger(isMoving ? "move" : "stop");
+            isAttacking = false;
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, followRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Vector3.zero + attackRangeOffset, attackRange);
+    }
+
+    public static Vector3 VectorMultiply (Vector3 A, Vector3 B)
+    {
+        return new Vector3(A.x * B.x, A.z * B.z, A.z * B.z);
     }
 }
